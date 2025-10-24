6b0f0d4c2728a28e826d184612fbcfee51e4a136
Karan2624 <karangupta@gmail.com>
2025-10-25 01:35:04 +0530

    added emailing feature

--- aifriendspliter/app/api/inngest/route.js
+++ aifriendspliter/app/api/inngest/route.js
@@ -1,11 +1,12 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
-import { helloWorld } from "@/lib/inngest/functions";
+import { paymentReminders } from "@/lib/inngest/payments-reminders";
+
 
 // Create an API that serves your Inngest functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    helloWorld, // include your function(s) here
+    paymentReminders,
   ],
 });
